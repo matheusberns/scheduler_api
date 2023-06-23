3cfbef90b315a896bb21fa59a4e4c97eba30ae9e
Berns <matheusberns@gmail.com>
2023-06-22 22:10:06 -0300

    migrações

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+::User.find_or_initialize_by(email: 'admin@bsystems.com.br').tap do |user|
+  user.name = 'Administrador'
+  user.password = '#Senha123'
+  user.password_confirmation = '#Senha123'
+  user.active = true
+  user.deleted_at = nil
+  user.save!
+end
