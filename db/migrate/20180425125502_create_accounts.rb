d61ec3017fca7b39a5f6812d286a6ef5abc4c281
Berns <matheusberns@gmail.com>
2023-07-26 23:26:04 -0300

    migrações

--- db/migrate/20180425125502_create_accounts.rb
+++ db/migrate/20180425125502_create_accounts.rb
@@ -1,6 +1,6 @@
 class CreateAccounts < ActiveRecord::Migration[6.1]
   def change
-    enable_extension 'unaccent'
+    # enable_extension 'unaccent'
     enable_extension 'uuid-ossp'
     enable_extension 'pgcrypto'
 
