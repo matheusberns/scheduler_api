16664f6778144df911f388bb8155bb6dc4f66634
Berns <matheusberns@gmail.com>
2023-08-26 14:58:04 -0300

    active storage

--- app/services/ibge/state.rb
+++ app/services/ibge/state.rb
@@ -1,26 +1,23 @@
 # frozen_string_literal: true
+class Ibge::State < Base
+  def find
+    request = self.class.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados', @options)
+    return unless request.ok?
 
-module Ibge
-  class State < Base
-    def find
-      request = self.class.get('/localidades/estados', @options)
-      return unless request.ok?
+    @ibge_states = request.parsed_response.map(&:deep_symbolize_keys!)
+  end
 
-      @ibge_states = request.parsed_response.map(&:deep_symbolize_keys!)
+  def import
+    @ibge_states.each do |ibge_state|
+      state = ::Region::State.activated.find_or_initialize_by(uf: ibge_state[:sigla])
+      state.name = ibge_state[:nome]
+      state.save
     end
+  end
 
-    def import
-      @ibge_states.each do |ibge_state|
-        state = ::Region::State.activated.find_or_initialize_by(uf: ibge_state[:sigla])
-        state.name = ibge_state[:nome]
-        state.save
-      end
-    end
+  private
 
-    private
-
-    def initialize
-      @options = { headers: { 'Content-Type' => 'application/json' } }
-    end
+  def initialize
+    @options = { headers: { 'Content-Type' => 'application/json' } }
   end
 end
