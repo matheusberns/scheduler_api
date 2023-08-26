e87bb13a9394892583952f4216823d17814bd597
Berns <matheusberns@gmail.com>
2023-08-26 14:41:30 -0300

    active storage

--- app/controllers/account_admins/headquarters_controller.rb
+++ app/controllers/account_admins/headquarters_controller.rb
@@ -2,35 +2,123 @@
 
 module AccountAdmins
   class HeadquartersController < ::ApiController
-    before_action :set_headquarter, only: :show
+    before_action :set_headquarter, only: %i[show update destroy images]
 
     def index
-      @headquarters = @current_user.account.headquarters.list
+      @headquarters = @account.headquarters.list
 
-      @headquarters = apply_filters(@headquarters, :by_cnpj,
-                                 :by_name)
+      @headquarters = apply_filters(@headquarters, :active_boolean,
+                                    :by_name,
+                                    :by_nickname,
+                                    :by_social_reason,
+                                    :by_cnpj)
 
-      render_index_json(@headquarters, ::Homepages::Headquarters::IndexSerializer, 'headquarters')
+      render_index_json(@headquarters, Headquarters::IndexSerializer, 'headquarters')
     end
 
     def autocomplete
-      @headquarters = @current_user.account.headquarters.autocomplete
-
-      @headquarters = @headquarters.by_id(@headquarter_ids) if @headquarter_ids.any?
+      @headquarters = @account.headquarters.autocomplete
 
       @headquarters = apply_filters(@headquarters, :active_boolean, :by_search)
 
-      render_index_json(@headquarters, ::Homepages::Headquarters::AutocompleteSerializer, 'headquarters')
+      render_index_json(@headquarters, Headquarters::AutocompleteSerializer, 'headquarters')
     end
 
     def show
-      render_show_json(@headquarter, ::Homepages::Headquarters::ShowSerializer, 'headquarter', 200)
+      render_show_json(@headquarter, Headquarters::ShowSerializer, 'headquarter')
+    end
+
+    def create
+      @headquarter = @account.headquarters.new(headquarter_create_params)
+
+      if @headquarter.save
+        render_show_json(@headquarter, Headquarters::ShowSerializer, 'headquarter', 201)
+      else
+        render_errors_json(@headquarter.errors.messages)
+      end
+    end
+
+    def update
+      if @headquarter.update(headquarter_update_params)
+        render_show_json(@headquarter, Headquarters::ShowSerializer, 'headquarter')
+      else
+        render_errors_json(@headquarter.errors.messages)
+      end
+    end
+
+    def destroy
+      if @headquarter.destroy
+        render_success_json
+      else
+        render_errors_json(@headquarter.errors.messages)
+      end
+    end
+
+    def recover
+      @headquarter = @account.headquarters.list.active(false).find(params[:id])
+
+      if @headquarter.recover
+        render_show_json(@headquarter, Headquarters::ShowSerializer, 'headquarter')
+      else
+        render_errors_json(@headquarter.errors.messages)
+      end
+    end
+
+    def images
+      if @headquarter.update(images_params)
+        render_show_json(@headquarter, Headquarters::ShowSerializer, 'headquarter')
+      else
+        render_errors_json(@headquarter.errors.messages)
+      end
+    end
+
+    def consult_cnpj
+      @consult = ::Consults::Cnpj.new(cnpj: params[:cnpj])
+      @consult.get
+
+      render_success_json(body: @consult.headquarter_object)
     end
 
     private
 
     def set_headquarter
-      @headquarter = @current_user.account.headquarters.show.find(params[:id])
+      @headquarter = @account.headquarters.activated.list.find(params[:id])
+    end
+
+    def headquarter_create_params
+      headquarter_params.merge(created_by_id: @current_user.id)
+    end
+
+    def headquarter_update_params
+      headquarter_params.merge(updated_by_id: @current_user.id)
+    end
+
+    def images_params
+      params
+        .require(:headquarter)
+        .permit(:logo)
+        .transform_values do |value|
+        value == 'null' ? nil : value
+      end
+        .merge(skip_validation: true)
+    end
+
+    def headquarter_params
+      params
+        .require(:headquarter)
+        .permit(:name,
+                :social_reason,
+                :nickname,
+                :cnpj,
+                :zipcode,
+                :address,
+                :address_number,
+                :address_complement,
+                :email,
+                :phone,
+                :secondary_phone,
+                :state_id,
+                :city_id)
     end
   end
 end
