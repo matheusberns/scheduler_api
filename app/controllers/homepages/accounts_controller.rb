03b7c2f7b5b7d53f89b279b856060de0afb0ace7
Berns <matheusberns@gmail.com>
2023-08-31 22:55:16 -0300

    active storage

--- app/controllers/homepages/accounts_controller.rb
+++ app/controllers/homepages/accounts_controller.rb
@@ -2,7 +2,6 @@
 
 module Homepages
   class AccountsController < ::ApplicationController
-    include Renders
     before_action :set_account, only: %i[show]
 
     def show
@@ -13,6 +12,23 @@ module Homepages
 
     def set_account
       @account = Account.find_by(base_url: params[:subdominio])
+    end
+
+    def render_show_json(object, serializer, return_name, status = nil, custom_params = nil)
+      object_all_fields = object.class.show.find(object.id)
+      status ||= 200
+      fields = params[:attributes].split(',') if params[:attributes].present?
+
+      return_name = return_name.camelize(:lower) if params[:key_transform_camel_lower]
+
+      render json: {
+        return_name => ActiveModelSerializers::SerializableResource.new(object_all_fields, {
+          serializer: serializer,
+          params: custom_params,
+          fields: fields,
+          key_transform: (params[:key_transform_camel_lower] ? 'camel_lower' : nil)
+        }).as_json
+      }, status: status
     end
   end
 end
