fccffadf966157736f881cff4abeb7ee4996fc0a
Berns <matheusberns@gmail.com>
2023-09-10 23:20:51 -0300

    enum de status de agendamento

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-DEFAULT_PW = 'Senha123'
 AUTH_KEY = 'f4f49fd6deed76678e60'
 CONSULT_IBGE_URL = 'https://servicodados.ibge.gov.br/api/v1'
 # BASE_URL = 'scheduler.bsystems.com.br'
