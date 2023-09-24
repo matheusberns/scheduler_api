665b410761e60d8bc144fba7d4f53afe34c7e2a4
matheus <M@th3us>
2023-09-24 12:47:59 -0300

    cadastro agendamento abaixo de filial

--- app/controllers/homepages/schedules_controller.rb
+++ app/controllers/homepages/schedules_controller.rb
@@ -2,10 +2,11 @@
 
 module Homepages
   class SchedulesController < ::ApiController
-    before_action :set_schedule, only: %i[show update destroy attachment_delete]
+    before_action :set_headquarter, only: %i[create show update destroy]
+    before_action :set_schedule, only: %i[show update destroy]
 
     def index
-      @schedules = @current_user.headquarter.schedules.list
+      @schedules = @headquarter.schedules.list
 
       @schedules = apply_filters(@schedules, :active_boolean,
                                  :by_schedule_id,
@@ -19,7 +20,7 @@ module Homepages
     end
 
     def create
-      @schedule = @current_user.headquarter.schedules.new(schedule_create_params)
+      @schedule = @headquarter.schedules.new(schedule_create_params)
 
       if @schedule.save
         render_show_json(@schedule, Schedules::ShowSerializer, 'schedule', 201)
@@ -47,7 +48,7 @@ module Homepages
     end
 
     def recover
-      @schedule = @current_user.schedules.list.active(false).find(params[:id])
+      @schedule = Schedule.list.active(false).find(params[:id])
 
       if @schedule.recover
         render_show_json(@schedule, Schedules::ShowSerializer, 'schedule')
@@ -59,11 +60,15 @@ module Homepages
     private
 
     def set_schedule
-      @schedule = @current_user.headquarter.schedules.find(params[:id])
+      @schedule = @headquarter.schedules.find(params[:id])
+    end
+
+    def set_headquarter
+      @headquarter = @account.headquarters.find(params[:id])
     end
 
     def schedule_create_params
-      schedule_params.merge(created_by_id: @current_user.id, account_id: @current_user.account_id, date: Time.now)
+      schedule_params.merge(created_by_id: @current_user.id, account_id: @current_user.account_id)
     end
 
     def schedule_update_params
