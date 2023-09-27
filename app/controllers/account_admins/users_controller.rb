5421d57a534905298d7de9f4df0825d850c01219
Matheus Berns <matheus.berns@joinin.com.br>
2023-09-26 21:44:34 -0300

    rota de cadastro de profissionais

---
+++ app/controllers/account_admins/users_controller.rb
@@ -0,0 +1,174 @@
+module AccountAdmins
+  class UsersController < ::ApiController
+    include CheckCurrentPassword
+    include PasswordChangeManageTokens
+    before_action :validate_permission, except: %i[show autocomplete]
+    before_action :validate_current_password, only: :update
+    before_action :set_user, only: %i[show update images approve_nickname reprove_nickname
+                                      update_salary_advance unlock_user remove_sessions create_facial_id validate_facial_id]
+
+    def index
+      @users = @account.users.list.not_account_administrator
+
+      @users = apply_filters(@users, :active_boolean,
+                             :by_name,
+                             :by_email,
+                             :by_cpf,
+                             :by_id,
+                             :by_phone_extension,
+                             :by_license_plate,
+                             :by_identification_number,
+                             :by_birthday_date_range,
+                             :by_department_id,
+                             :by_headquarter_id)
+
+      respond_to do |format|
+        format.json do
+          render_index_json(@users, Users::IndexSerializer, 'users')
+        end
+
+        format.xlsx do
+          users = sortable(@users)
+          file_url = ::Export::UserExcel.new(users).file_url
+          render json: { file: file_url }, status: :ok
+        end
+      end
+    end
+
+    def autocomplete
+      @users = @account.users.autocomplete
+
+      @users = apply_filters(@users, :active_boolean,
+                             :by_search,
+                             :by_attendance_category_id,
+                             :by_incident_location_id,
+                             :by_manager_id,
+                             :by_sale_type,
+                             :only_managers_boolean,
+                             :by_headquarter_id,
+                             :by_department_id,
+                             :by_manager_id,
+                             :by_job_title_id,
+                             :by_hide_specific_users,
+                             :by_shift_id)
+
+      render_autocomplete(@users, Users::AutocompleteSerializer, 'users')
+    end
+
+    def show
+      render_show_json(@user, Users::ShowSerializer, 'user', 200)
+    end
+
+    def create
+      @user = @account.users.new(user_create_params)
+
+      if @user.save
+        render_show_json(@user, Users::ShowSerializer, 'user', 201)
+      else
+        render_errors_json(@user.errors.messages)
+      end
+    end
+
+    def update
+      update_params = @user.identification_number? ? user_update_params : user_update_professional_params
+
+      if @user.update(update_params)
+        manage_current_tokens
+        render_show_json(@user, Users::ShowSerializer, 'user')
+      else
+        render_errors_json(@user.errors.messages)
+      end
+    end
+
+    def destroy
+      verify_if_is_current_user_being_destroyed
+      @user = @account.users.list.not_account_administrator.find(params[:id])
+
+      if @user.destroy
+        render_success_json
+      else
+        render_errors_json(@user.errors.messages)
+      end
+    end
+
+    def recover
+      @user = @account.users.list.not_account_administrator.active(false).find(params[:id])
+
+      if @user.recover
+        render_show_json(@user, Users::ShowSerializer, 'user')
+      else
+        render_errors_json(@user.errors.messages)
+      end
+    end
+
+    def images
+      if @user.update(images_params)
+        render_show_json(@user, Users::ShowSerializer, 'user')
+      else
+        render_errors_json(@user.errors.messages)
+      end
+    end
+
+    private
+
+    def validate_permission
+      render_error_json(status: 405) unless find_permission([::PermissionCodeEnum::USER_MANAGE])
+    end
+
+    def set_user
+      @user = @account.users.activated.list.find(params[:id])
+    end
+
+    def user_create_params
+      create_params.merge(created_by_id: @current_user.id)
+    end
+
+    def user_update_params
+      user_params.merge(updated_by_id: @current_user.id, skip_photo_validation: true)
+    end
+
+    def images_params
+      params
+        .require(:user)
+        .permit(:photo,
+                :background_profile_image)
+        .transform_values do |value|
+        value == 'null' ? nil : value
+      end
+    end
+
+    def user_params
+      params
+        .require(:user)
+        .permit(:phone,
+                :address,
+                :zipcode,
+                :city_id,
+                :nickname,
+                :password,
+                :state_id,
+                :cellphone,
+                :occupation,
+                :timeout_in,
+                :district_id,
+                :address_number,
+                :personal_email,
+                :phone_extension,
+                :address_complement,
+                :dont_show_birthday,
+                :force_change_password,
+                :password_confirmation)
+    end
+
+    def create_params
+      params
+        .require(:user)
+        .permit(:cpf,
+                :name,
+                :email,
+                :password,
+                :headquarter_id,
+                :password_confirmation)
+    end
+  end
+end
--- app/enumerations/permission_module_type_enum.rb
+++ app/enumerations/permission_module_type_enum.rb
@@ -1,14 +1,56 @@
-# frozen_string_literal: true
-
 class PermissionModuleTypeEnum < EnumerateIt::Base
   associate_values(
-    post: 1
+    post: 1,
+    group: 2,
+    department: 3,
+    restaurant: 4,
+    poll: 5,
+    corporate_tv: 6,
+    birthday_card: 7,
+    contact: 8,
+    shift: 9,
+    headquarter: 10,
+    user: 11,
+    space: 12,
+    useful_website: 13,
+    benefit: 14,
+    car: 15,
+    category_attendance: 16,
+    priority_attendance: 17,
+    attendance: 18,
+    solicitation: 19,
+    account: 20,
+    meeting_room: 21,
+    table: 22,
+    local: 23,
+    company_birthday_card: 24,
+    district: 25,
+    digital_magazine: 26,
+    visit: 27,
+    scholarship: 28,
+    training: 29,
+    calendar: 30,
+    contract: 31,
+    survey: 32,
+    job_title: 33,
+    employee_journey: 34,
+    message_submission: 35,
+    dashboard: 36,
+    dynamic_form: 37,
+    store: 38,
+    analysis_performance: 39,
+    use_term: 40,
+    hr: 41,
+    interactive_media: 42,
+    banner: 43,
+    incident: 44,
+    manager: 45
   )
 
   def self.name_ordered
     to_a
       .map { |name, id| { id: id, name: name } }
-      .sort_by { |object| object[:name] }
+      .sort_by { |object| I18n.transliterate(object[:name]) }
   end
 
   sort_by :value
