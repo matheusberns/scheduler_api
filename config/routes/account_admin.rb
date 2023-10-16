08a9688434c82d12efc5dae1ef3a5df05d4388ce
Matheus Berns <matheus.berns@joinin.com.br>
2023-10-15 22:56:47 -0300

    ajuste porta email

--- config/routes/account_admin.rb
+++ config/routes/account_admin.rb
@@ -1,7 +1,7 @@
   # frozen_string_literal: true
 
 ApiScheduler::Application.routes.draw do
-  root :to => 'home#index'
+  # root :to => 'home#index'
 
   scope module: :account_admins do
     match 'headquarters/autocomplete' => 'headquarters#autocomplete', via: :get
